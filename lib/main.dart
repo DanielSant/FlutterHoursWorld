5e40794923ef8b40ecaeb0bcae27d9f5fe3c1b93
DanielSant <daniel17sants@gmail.com>
2020-07-07 15:26:39 -0300

    Buttons and Icons

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,33 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Image.asset('assets/space-3.jpg'),
+        child: IconButton(
+          onPressed: () {
+            print('you clicked me');
+          },
+          icon: Icon(Icons.alternate_email),
+          color: Colors.amber,
+        ),
+//        child: RaisedButton.icon(
+//          onPressed: () {},
+//          icon: Icon(
+//            Icons.mail
+//          ),
+//          label: Text('mail me'),
+//          color: Colors.amber,
+//        ),
+//          child: FlatButton(
+//            onPressed: () {
+//              print('you clicked me');
+//            },
+//            child: Text('click me'),
+//            color: Colors.lightBlue,
+//          ),
+//        child: Icon(
+//          Icons.airport_shuttle,
+//          color: Colors.lightBlue,
+//          size: 50.0,
+//        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},

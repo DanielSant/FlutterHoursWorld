beffa21c193d97ed4d29c039f497ba6ec0915dab
DanielSant <daniel17sants@gmail.com>
2020-07-07 14:20:09 -0300

    Colors and Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,31 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text('My first App'),
+        title: Text(
+          'Hello World',
+          style: TextStyle(
+            fontSize: 20.0,
+          ),
+        ),
         centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text('Hello World!'),
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
+        onPressed: () {},
         child: Text('Click'),
+        backgroundColor: Colors.red[600],
       ),
     ),
   ));
